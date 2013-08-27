004a2aaada596ffc4e170d21291e0fde70ccaa61
VictorPacheco <vpacheco99@gmail.com>
2013-08-27 19:00:04 -0400

    Code up to 5th video

--- CourseSampleCode/HelloWorld/Program.cs
+++ CourseSampleCode/HelloWorld/Program.cs
@@ -9,8 +9,49 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("This is my first C# App");
+            #region FirstVideo
+            //Code from the first video
+            //Console.WriteLine("Hello World");
+            //Console.WriteLine("This is my first C# App");
+            #endregion
+
+            #region 3rdVideo
+            //Code from 3rd video
+            //Write more comments
+            string firstName = "John";
+            int age = 12;
+            decimal salary = 34.67m;
+
+            //Console.WriteLine(firstName + " " + age + " "+ salary);
+            #endregion
+
+            //4th video about how to add comments to your code
+
+            //5th video - Loops
+            //for loop
+            //for (int i = 1; i < 21; i++)
+            //{
+            //    Console.WriteLine("This is a for loop " + i);
+            //}
+
+            string[] list = new string[]
+                                {
+                                    "Hello",
+                                    "Bye",
+                                    "Dinner Time"
+                                };
+
+            //foreach (string item in list)
+            //{
+            //    Console.WriteLine("This is a for each loop " + item);
+            //}
+
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine("A while loop " + counter);
+                counter++;
+            }
             Console.ReadLine();
         }
     }
