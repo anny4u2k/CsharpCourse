bcf192ee8616f16f9f3af3f684dc6e11fd0be20f
VictorPacheco <vpacheco99@gmail.com>
2013-08-27 17:53:16 -0400

    Sample Code
    Hello World

---
+++ CourseSampleCode/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("This is my first C# App");
+            Console.ReadLine();
+        }
+    }
+}
